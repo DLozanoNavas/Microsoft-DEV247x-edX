37bc622d581b3675d3ddc70adbc263f402a4ac5d
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-05-06 11:54:04 -0500

    Adds demo 1: C Sharp Serialize Deserialize

---
+++ SerializeDeserialize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializeDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
