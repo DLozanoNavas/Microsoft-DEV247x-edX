9dedd99766ed2cc02a1a8ab9bd3364912433cf42
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-05-06 12:22:51 -0500

    Adds deserialization JS

--- SerializeDeserializeJS/main.js
+++ SerializeDeserializeJS/main.js
@@ -1,6 +1,13 @@
 // Model object
-var product ={Id:1, name:"Car", Price:2000.9}
+var product = {Id:1, Name:"Car", Price:2000.9}
 
 var JsonString = JSON.stringify(product);
 document.write(JsonString);
 document.write("<br/>");
+
+var str = '{"Id":1,"Name":"Car","Price":2000.9}';
+
+var ProductDeserialized = JSON.parse(str);
+document.write("Product ID: "+ ProductDeserialized.Id+"<br/>");
+document.write("Product Name: "+ ProductDeserialized.Name+"<br/>");
+document.write("Product Price: "+ ProductDeserialized.Price+"<br/>");
