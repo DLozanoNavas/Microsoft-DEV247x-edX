de77fa3de81d0b9b3cd019ccf47be61bbd51e488
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-06-11 12:39:49 -0500

    [Fix] Not buggy anymore :)

--- Lab2_REST_HTTP/Use Postman Create HTTP Requests/Debug Web API with Postman/Buggy/WebServer/Models/Repository.cs
+++ Lab2_REST_HTTP/Use Postman Create HTTP Requests/Debug Web API with Postman/Buggy/WebServer/Models/Repository.cs
@@ -5,7 +5,7 @@ namespace WebServer.Models {
         public static IDictionary<int, Product> Products { get; private set; }
 
         static Repository() {
-            var Products = new Dictionary<int, Product>();
+            Products = new Dictionary<int, Product>();
             Products[0] = new Product { ID = 0, Name = "Apple", Price = 1.99 };
             Products[0] = new Product { ID = 1, Name = "Bike", Price = 99.99 };
             Products[2] = new Product { ID = 3, Name = "Donuts", Price = 2.99 };
